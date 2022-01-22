deab9fcdc56d3110fec755ab4fc84288fa8a6978
Harry O'Connor <harryoconnorwork@gmail.com>
2022-01-22 14:17:03 +0000

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+console.log("Hello world")
+
+d3.selectAll("p").style("color", "blue");
+
+const svg = d3.select('svg')
+svg.style('background-color', 'red')
+
+d3.csv('data/data.csv').then(data =>{
+    console.log(data)
+})
