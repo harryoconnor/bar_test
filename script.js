1ca86ad5ad332b59f68f390ad202579841ec6c4e
Harry O'Connor <harryoconnorwork@gmail.com>
2022-01-22 17:15:29 +0000

    test stuff

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 
 
-console.log("Hello world")
-
-d3.selectAll("p").style("color", "blue");
-
 const svg = d3.select('svg')
+
 svg.style('background-color', 'red')
 
-console.log("test2")
+const render = data => {
+    const scale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.Score)])
+        .range(0,300);
+    
+    console.log(scale.domain())
+
+    svg.selectAll('rect').data(data)
+        .enter().append('rect')
+        .attr('width', d=>scale(d.Score))
+        .attr('height',100)
+}
 
 d3.csv('data/data.csv').then(data =>{
+    data.forEach(d=>{
+        console.log(d)
+        d.Score = +d.Score;
+    });
     console.log(data)
+    render(data)
 })
