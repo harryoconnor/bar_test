af880db5e98615c28737af9826167915c5b5ae69
Harry O'Connor <harryoconnorwork@gmail.com>
2022-01-22 14:28:46 +0000

    quick

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ d3.selectAll("p").style("color", "blue");
 const svg = d3.select('svg')
 svg.style('background-color', 'red')
 
-console.log("test")
+console.log("test2")
 
 d3.csv('data/data.csv').then(data =>{
     console.log(data)
