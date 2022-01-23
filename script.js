2c0989757f783874d64fa4507b1c38d6f52fc3e4
Harry O'Connor <harryoconnorwork@gmail.com>
2022-01-23 12:41:30 +0000

    finished bar chart

--- script.js
+++ script.js
@@ -2,26 +2,63 @@
 
 const svg = d3.select('svg')
 
-svg.style('background-color', 'red')
+svg.style('background-color', '#FAF9F6')
 
 const render = data => {
-    const scale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.Score)])
-        .range([0,300]);
-    
-    console.log(scale.domain())
+    const x_value = d => d.Score;
+    const y_value = d => d.Game
+    width = parseInt(svg.style("width"));
+    height = parseInt(svg.style("height"));
 
-    svg.selectAll('rect').data(data)
+    const margin = {
+        top:20,
+        right:20,
+        bottom:30,
+        left:80
+    }
+
+    const inner_width = width - margin.right - margin.left
+    const inner_height = height - margin.top - margin.bottom
+
+    const x_scale = d3.scaleLinear()
+        .domain([0, d3.max(data, x_value)])
+        .range([0,inner_width]);
+
+
+    const y_scale = d3.scaleBand()
+    .domain(data.map(y_value))
+    .range([0,inner_height])
+    .padding(0.1)
+
+    
+    console.log(y_scale.domain())
+    //${margin.left}
+
+
+    const bar_chart_group = svg.append('g')
+        .attr('transform', `translate(${margin.left},${margin.right})`)
+
+
+    bar_chart_group.append('g').call(d3.axisLeft(y_scale))
+
+    bar_chart_group.append('g').call(d3.axisBottom(x_scale))
+        .attr('transform', `translate(0,${inner_height})` );
+
+    bar_chart_group.selectAll('rect').data(data)
         .enter().append('rect')
-        .attr('width', d=>scale(d.Score))
-        .attr('height',100)
+        .attr('y',d =>y_scale(y_value(d)))
+        .attr('width', d=>x_scale(x_value(d)))
+        .attr('height',y_scale.bandwidth())
 }
 
-d3.csv('data/data.csv').then(data =>{
+process_data = data =>{
     data.forEach(d=>{
-        console.log(d)
         d.Score = +d.Score;
     });
     console.log(data)
     render(data)
-})
+}
+
+
+d3.csv('data/data.csv').then(process_data)
+
